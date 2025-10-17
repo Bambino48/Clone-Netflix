ededae8b5108d899254c17eeb4d42d498450bfb5
Ibrahim Bamba <ibamba366@gmail.com>
2025-10-17 11:49:05 +0000

    Debut du projet et creation de la page home avec la navbar

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>© 2023 Clone Netflix. All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
