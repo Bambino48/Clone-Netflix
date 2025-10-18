b53aeba4c4508613ddaf774dba18874ef90a75d5
Ibrahim Bamba <ibamba366@gmail.com>
2025-10-18 01:04:52 +0000

    Integration de la fonction player et lecture via id

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import "./Player.scss";
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [apiData, setApiData] = useState({
+        name:"",
+        key:"",
+        published_at:"",
+        type:""
+    });
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjE0Y2NhYmVmYjhhYTVlMGY3MzRlZDkxNWZkYjcwZCIsIm5iZiI6MTc2MDc0Mjc4OS40MzgwMDAyLCJzdWIiOiI2OGYyY2Q4NWYwZDU3YTM3YzMzOTEyOWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zcoJsNzPpvgNohIf29KreAWz5DwI23yHVn5HT9MZskc'
+        }
+    };
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+        .then(res => res.json())
+        .then(res => setApiData(res.results[0]))
+        .catch(err => console.error(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div className="player">
-            <h1>Player</h1>
+            <img src={back_arrow_icon} alt="Back" onClick={() => navigate(-2)}/>
+            <iframe width='90%' height='90' src={`https://www.youtube.com/embed/${apiData.key}`} title="Amine" frameBorder='0' allowFullScreen></iframe>
+            <div className="player-info">
+                <p>{apiData.published_at.slice(0, 10)}</p>
+                <p>{apiData.name}</p>
+                <p>{apiData.type}</p>
+            </div>
         </div>
     );
 };
